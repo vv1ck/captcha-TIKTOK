03900a31f1273991e181ea44c200aec2d8ee0aa8
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-08-12 04:18:32 +0300

    Create Login-api.py

---
+++ Login-api.py
@@ -0,0 +1,4 @@
+# Url login api TikTok 
+
+
+url= f'https://api2.musical.ly/passport/user/login/?username=&email={email}&mobile=&account=&password={pass}&captcha=&iid=6967491361062930181&channel=googleplay&device_type=SM-G617&os_version=11&version_code=820&app_name=musical_ly&device_platform=android'
